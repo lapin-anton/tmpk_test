bc952a6c0cabf5c484f34bf0d55991374a00fad5
lapin-anton <lapin_antoine@mail.ru>
2021-01-21 16:49:01 +0300

    add db scripts

---
+++ db_scripts/address_fill.sql
@@ -0,0 +1,3 @@
+use tmpk;
+INSERT INTO `tmpk`.`address` (`city`, `street`, `building`, `apartment`) VALUES ('Дубна', 'Энтузиастов', '34', '105');
+INSERT INTO `tmpk`.`address` (`city`, `street`, `building`, `apartment`) VALUES ('Дубна', 'Строителей', '21', '1');
---
+++ db_scripts/tariff_fill.sql
@@ -0,0 +1,3 @@
+use tmpk;
+INSERT INTO `tmpk`.`tariff` (`name`, `cost`, `start_date`, `finish_date`) VALUES ('Max50', '350.00', '2020-05-08', '2025-05-08');
+INSERT INTO `tmpk`.`tariff` (`name`, `cost`, `start_date`, `finish_date`) VALUES ('Max100', '600', '2020-07-01', '2025-07-01');
