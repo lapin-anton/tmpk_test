1f22f5fc86c28a914c1a6a5cfa153f57a7333bd8
lapin-anton <75936838+lapin-anton@users.noreply.github.com>
2021-01-21 14:11:03 +0300

    Add sql files of application's data base

---
+++ incoming_fill.sql
@@ -0,0 +1,7 @@
+use tmpk;
+INSERT INTO `tmpk`.`incoming` (`amount`, `date`, `contract_id`) VALUES ('350', '2020-06-02', '1');
+INSERT INTO `tmpk`.`incoming` (`amount`, `date`, `contract_id`) VALUES ('350', '2020-07-02', '1');
+INSERT INTO `tmpk`.`incoming` (`amount`, `date`, `contract_id`) VALUES ('350', '2020-08-02', '1');
+INSERT INTO `tmpk`.`incoming` (`amount`, `date`, `contract_id`) VALUES ('600', '2020-08-04', '2');
+INSERT INTO `tmpk`.`incoming` (`amount`, `date`, `contract_id`) VALUES ('350', '2020-09-02', '1');
+INSERT INTO `tmpk`.`incoming` (`amount`, `date`, `contract_id`) VALUES ('600', '2020-09-05', '2');
---
+++ status_fill.sql
@@ -0,0 +1,3 @@
+use tmpk;
+INSERT INTO `tmpk`.`status` (`name`) VALUES ('Активный');
+INSERT INTO `tmpk`.`status` (`name`) VALUES ('Аннулирован');
